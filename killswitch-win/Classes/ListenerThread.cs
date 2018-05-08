6bdfe5da62743aab4c96f647040cbb716ebfab0b
Daniel Skovli <daniel.skovli@gmail.com>
2018-05-08 18:05:23 +1000

    Some blind changes to the Win build made on a Mac. Test plz

--- killswitch-win/Classes/ListenerThread.cs
+++ killswitch-win/Classes/ListenerThread.cs
@@ -13,6 +13,7 @@ namespace Killswitch.Classes {
 
 		bool debug = false;
 		int listenInterval = 3000;
+		URLs urls = new URLs();
 
 		public void Listen() {
 
@@ -33,15 +34,70 @@ namespace Killswitch.Classes {
 				Iterate();
 			}
 
-			// All good, lets get some JSON
-			DebugLog("Running");
-			Settings.Default.statusText = "System running";
+			// Talk to server
+			using (var webClient = new WebClient()) {
+				webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
 
+				try {
+					// Success
+					var response = webClient.DownloadString(urls.Status + Settings.Default.token);
+					var json = JsonConvert.DeserializeObject<Dictionary<string, object>>(response);
+					
+					DebugLog("Running");
+					Settings.Default.statusText = "System running";
 
-			Thread.Sleep(listenInterval);
-			Iterate();
+					// Take action if killswitch is set
+					if ((bool)json["killswitch"]) {
+						switch (Settings.Default.killswitchAction) {
 
+							// System sleep
+							case "Sleep":
+								DebugLog("Initiating sleep");
+								Sleep();
+								break;
+							
+							// System shutdown
+							case "Shutdown":
+								DebugLog("Initiating system shutdown");
+								// SAVE STUFF AND WHATEVER TO PREPARE
+								Settings.Default.Save();
+								Shutdown();
+								break;
+							
+							// System lock & catch-all in case our config is fucked
+							default:
+								DebugLog("Locking system");
+								Lock();
+								break;
+						}
+					}
 
+				} catch (WebException err) {
+
+					// Authentication error
+					if (err.Response != null) {
+						var response = new StreamReader(err.Response.GetResponseStream()).ReadToEnd();
+						var json = JsonConvert.DeserializeObject<Dictionary<string, object>>(response);
+						if (json.ContainsKey("error")) {
+							// Application.Current.Dispatcher.Invoke(new Action(() => {
+								Settings.Default.authenticated = false;
+								ThreadHelper.Run = false;
+							// }));
+						} else {
+							// Weird error, possibly server issue. Keep running
+							Settings.Default.statusText = "Server error";
+						}
+
+					// Server or network error
+					} else {
+						// Keep running
+						Settings.Default.statusText = "Network error";
+					}
+				} finally {
+					Thread.Sleep(listenInterval);
+					Iterate();
+				}
+			}
 		}
 
 		public void Iterate() {
